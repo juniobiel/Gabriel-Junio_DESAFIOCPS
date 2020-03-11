aa528386300a098fd79bfaafb21a2362b612c9d5
Gabriel Júnio <gabriel.junio@rocketmail.com>
2020-03-11 16:09:08 -0300

    Config db
    Configurações do Cloud SQL para teste

--- models/db.js
+++ models/db.js
@@ -1,8 +1,11 @@
 const Sequelize = require('sequelize');
 
-const sequelize =  new Sequelize('desafiocps', 'root', 'admin', {
-    host: 'localhost',
-    dialect: 'mysql'
+const sequelize =  new Sequelize('desafiocps', 'root', 'admin24', {
+    host: '/cloudsql/${desafiocps:southamerica-east1:desafio-cps}',
+    dialect: 'mysql',
+    dialectOption: {
+        socketPath: '/cloudsql/${desafiocps:southamerica-east1:desafio-cps}'
+    }
 });
 
 module.exports = {
