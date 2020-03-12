0cada29162bf45fb5082820c7139cea38f276b79
Gabriel Júnio <gabriel.junio@rocketmail.com>
2020-03-11 21:28:05 -0300

    db config
    Teste para config do db

--- models/db.js
+++ models/db.js
@@ -1,11 +1,9 @@
 const Sequelize = require('sequelize');
 
+
 const sequelize =  new Sequelize('desafiocps', 'root', 'admin24', {
     host: '/cloudsql/${desafiocps:southamerica-east1:desafio-cps}',
     dialect: 'mysql',
-    dialectOption: {
-        socketPath: '/cloudsql/${desafiocps:southamerica-east1:desafio-cps}'
-    }
 });
 
 module.exports = {
