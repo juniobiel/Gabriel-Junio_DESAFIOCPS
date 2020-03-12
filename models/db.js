a07deda1f5777ae3181066f7e82c67d1e58ef3d2
Gabriel Júnio <gabriel.junio@rocketmail.com>
2020-03-12 11:00:23 -0300

    Fix db e package
    Start script

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 
 const sequelize =  new Sequelize('desafiocps', 'desafiocps', 'admin2440', {
-    host: 'mysql669.umbler.com:41890',
+    host: 'mysql669.umbler.com',
     dialect: 'mysql',
 });
 
